62205377a6200570a418e9d4c43f804794716a0e
demo <demo@asd.virtfan.net>
2013-06-20 14:55:36 +0800

    update

--- vm_webadmin/instance/admin.py
+++ vm_webadmin/instance/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from instance.models import CommonOptions
 
 admin.site.register(CommonOptions)
+
+class vmadmin(admin.ModelAdmin):
+	list_display=('vmname','createdate')
--- vm_webadmin/instance/models.py
+++ vm_webadmin/instance/models.py
@@ -2,20 +2,19 @@ from django.db import models
 
 # Create your models here.
 class CommonOptions(models.Model):
-	vmname = models.CharField(max_length=255)
+	vmname = models.CharField('VM Name',max_length=255)
 	createdate = models.DateTimeField('Date created')
-class Student(models.Model):
-    FRESHMAN = 'FR'
-    SOPHOMORE = 'SO'
-    JUNIOR = 'JR'
-    SENIOR = 'SR'
-    YEAR_IN_SCHOOL_CHOICES = (
-        (FRESHMAN, 'Freshman'),
-        (SOPHOMORE, 'Sophomore'),
-        (JUNIOR, 'Junior'),
-        (SENIOR, 'Senior'),
-    )
-    year_in_school = models.CharField(max_length=2,
-                                      choices=YEAR_IN_SCHOOL_CHOICES,
-                                      default=FRESHMAN)
+    	Linux = 'linux'
+	Windows = 'windows'
+	Linux_x64 = 'linux_x64'
+	Windows_x64 = 'windows_x64'
+	OS = (
+        (Linux, 'linux'),
+	(Windows, 'windows'),
+        (Linux_x64, 'linux_x64'),
+	(Windows_x64, 'windows_x64'),
+	)
+    	useros = models.CharField(max_length=64,
+                                      choices=OS,
+                                      default=Linux_x64)
 
